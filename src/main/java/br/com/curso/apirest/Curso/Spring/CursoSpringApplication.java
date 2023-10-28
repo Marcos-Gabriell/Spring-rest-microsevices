6c72899febb0aeda75ca958ca1423351e6144c5f
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-28 18:35:54 -0300

    Initial commit

---
+++ src/main/java/br/com/curso/apirest/Curso/Spring/CursoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.curso.apirest.Curso.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+}

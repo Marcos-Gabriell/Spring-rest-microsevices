f9156242ac07aae67cdc04203a53379d2c0bbb69
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-31 11:11:24 -0300

    Modificando mapeamento

--- src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
+++ src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
@@ -3,10 +3,12 @@ package br.com.curso.apirest.Curso.Spring.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping(value = "/usuario")
 public class IndexController {
 
     @GetMapping(value = "/", produces = "application/json")

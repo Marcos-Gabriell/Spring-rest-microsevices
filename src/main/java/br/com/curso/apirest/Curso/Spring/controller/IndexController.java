b03f0c8a2e779fb8565c4e56ca319eefb86e89cf
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-31 21:38:13 -0300

    Modificando mapeamento

--- src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
+++ src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping(value = "/", produces = "application/json")
-    public ResponseEntity init() {
-        return new ResponseEntity("Olá REST", HttpStatus.OK);
+    public ResponseEntity init(@RequestParam(value = "nome") String nome) {
+
+        System.out.println("parametro sendo recebido " +  nome);
+
+        return new ResponseEntity("Olá REST seu nome é: " + nome, HttpStatus.OK);
     }
 }

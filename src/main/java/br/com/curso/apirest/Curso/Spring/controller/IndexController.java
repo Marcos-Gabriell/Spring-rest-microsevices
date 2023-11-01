f018f995d70fafc5b2a9f59e7d0f5b04f34f168d
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-11-01 15:13:25 -0300

    Colocando para aparcer nome default

--- src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
+++ src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping(value = "/", produces = "application/json")
-    public ResponseEntity init(@RequestParam(value = "nome") String nome) {
+    public ResponseEntity init(@RequestParam(value = "nome", defaultValue = "João Nome default") String nome) {
 
         System.out.println("parametro sendo recebido " +  nome);
 

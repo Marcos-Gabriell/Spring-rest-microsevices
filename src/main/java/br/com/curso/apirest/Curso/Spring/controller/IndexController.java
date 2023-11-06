cdca435a69e581a2467a056b8d5207c2e1559c91
Marcos <marcosgabriel79355@gmail.com>
2023-11-06 19:31:42 -0300

    Adicionando o repository ao controller

--- src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
+++ src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
@@ -1,6 +1,7 @@
 package br.com.curso.apirest.Curso.Spring.controller;
 
 import br.com.curso.apirest.Curso.Spring.model.Usuario;
+import br.com.curso.apirest.Curso.Spring.repository.UsuarioRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,8 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/usuario")
 public class IndexController {
+
+    private UsuarioRepository usuarioRepository;
 
     @GetMapping(value = "/", produces = "application/json")
     public ResponseEntity<Usuario> init() {

95755270899bc40240ba0cc7adc8189415bd8c3e
Marcos <marcosgabriel79355@gmail.com>
2023-11-06 15:13:00 -0300

    Começando a recber json

--- src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
+++ src/main/java/br/com/curso/apirest/Curso/Spring/controller/IndexController.java
@@ -13,9 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
 
     @GetMapping(value = "/", produces = "application/json")
-    public ResponseEntity init(@RequestParam(value = "nome", required = true, defaultValue = "Nome não informado") String nome, @RequestParam("salario") Long salario) {
+    public ResponseEntity init() {
 
-        System.out.println("parametro sendo recebido " +  nome);
+
 
         return new ResponseEntity("Olá REST seu nome é: " + nome + " salario é: " + salario, HttpStatus.OK);
     }

6fccd37839862a395b70b150caa4990dc8065124
Marcos <marcosgabriel79355@gmail.com>
2023-11-13 19:25:46 -0300

    Começando a criar relacionamento de um para muitos

---
+++ src/main/java/br/com/curso/apirest/Curso/Spring/model/Telefone.java
@@ -0,0 +1,12 @@
+package br.com.curso.apirest.Curso.Spring.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Telefone {
+
+    @Id
+    private Long id;
+
+}
